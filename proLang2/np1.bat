264320c278643a7095af26edf53915956ec3c4ce
Sanket Dhengre <83276393+SanketDhengre@users.noreply.github.com>
2024-10-12 23:44:35 +0530

    Add files via upload

---
+++ proLang2/np1.bat
@@ -0,0 +1,4 @@
+@echo off
+echo New Programming Language
+echo Made By Sanket Dhengre
+java -cp antlr-3.2.jar org.antlr.Tool Npl.g
---
+++ proLang2/npl2.bat
@@ -0,0 +1,4 @@
+@echo off
+echo New Programming Language
+echo Made By Sanket Dhengre
+javac -cp .;antlr-3.2.jar NplParser.java NplLexer.java
